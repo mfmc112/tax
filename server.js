d70c8eaf35920e6fcd4ff0e7de37e5d5da57b3fd
Marcos Costa <mfmc112@gmail.com>
2017-07-31 22:37:19 -0500

    changed server port configuration to environment variable

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
+var port = process.env.PORT || 5000
 
 server.use(express.static(__dirname + '/project'));
 server.use(bodyParser.json());
 server.use(bodyParser.json({type: 'application/vdn.api+json'}));
 
-server.listen();
+server.listen(port);
 console.log("Tax Smart is up");
 
 //application
