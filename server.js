fd7b484a0836a15ad91e45a97f83208d6c1c2bd6
Marcos Costa <mfmc112@gmail.com>
2017-07-31 22:32:35 -0500

    removed server port configuration

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var server = express();
 var bodyParser = require('body-parser');
-var port = 80;
 
 server.use(express.static(__dirname + '/project'));
 server.use(bodyParser.json());
 server.use(bodyParser.json({type: 'application/vdn.api+json'}));
 
-server.listen(port);
-console.log("Blog is up on port " + port);
+server.listen();
+console.log("Tax Smart is up");
 
 //application
 server.get('*', function(req,res) {
