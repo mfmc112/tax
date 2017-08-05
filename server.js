5207f519f97e5834815a310fcc00fd562b02553b
Marcos Costa <mfmc112@gmail.com>
2017-08-04 21:01:34 -0500

    configuring to run PROD

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var server = express();
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 5000
 
-server.use(express.static(__dirname + '/src'));
+server.use(express.static(__dirname + '/dist'));
 server.use(bodyParser.json());
 server.use(bodyParser.json({type: 'application/vdn.api+json'}));
 
@@ -12,5 +12,5 @@ console.log("Tax Smart is up on port " + port);
 
 //application
 server.get('*', function(req,res) {
-  res.sendfile('./src/index.html');
+  res.sendfile('./dist/index.html');
 });
