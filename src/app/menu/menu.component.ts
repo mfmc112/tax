b36103480a8c3fd8c53a74b04b3c79ce5b865bc4
Marcos Costa <mfmc112@gmail.com>
2017-08-20 20:03:15 -0500

    loading  landing-page after login

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
+import { UIRouterModule, UIRouter } from '@uirouter/angular';
 import { CommonService } from '../common.service';
 import { User } from '../login/user';
 
@@ -11,10 +12,18 @@ export class MenuComponent implements OnInit, DoCheck {
 
   user: User;
 
-  constructor ( private commonService: CommonService ) {}
+  constructor (
+    private _uiRouter: UIRouter,
+    private commonService: CommonService ) {}
 
   ngOnInit(): void {
-    this.user = this.commonService.getUser();
+    if (!this.commonService.getUser()) {
+      this._uiRouter.stateService.go('login');
+    } else {
+      this.user = this.commonService.getUser();
+      this._uiRouter.stateService.go('menu.landingPage');
+
+    }
   }
 
   // This ebables two way binding so when user changes into commonService it will get it
