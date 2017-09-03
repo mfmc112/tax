7451679dddddff8d75ddce9571fb14a79680a690
Marcos Costa <mfmc112@gmail.com>
2017-09-03 01:18:25 -0500

    added missing fields 13

--- src/app/application-forms/utils/form-sub-header.component.ts
+++ src/app/application-forms/utils/form-sub-header.component.ts
@@ -5,7 +5,7 @@ import { Component, Input } from '@angular/core';
   inputs:['nTitle'],
   template: `
 
-    <div class="divider-50"></div>
+    <div class="divider-20"></div>
     <p class="lead">{{nTitle}}</p>
     <hr class="sub-header-hr">
   `
