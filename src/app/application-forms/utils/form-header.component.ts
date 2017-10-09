5c90a18bbf5c593f5c9f069f27405b454038c5b9
Marcos Costa <mfmc112@gmail.com>
2017-10-08 22:52:26 -0500

    Forms header and amount box layout changed

--- src/app/application-forms/utils/form-header.component.ts
+++ src/app/application-forms/utils/form-header.component.ts
@@ -2,14 +2,27 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'form-header',
-  inputs:['nTitle', 'nSubTitle'],
+  inputs:['nTitle', 'nSubTitle', 'nYear'],
   template: `
-    <div class="page-header" style="background-color:#cccccc; margin-bottom: 20px;">
-        <h1>US<small><small> {{nTitle}} <small *ngIf="nSubTitle"><small>{{nSubTitle}}</small></small></small></small></h1>
+    <div class="page-header" style="background-color:#cccccc; margin-bottom:20px; line-height:1; color:#004d66">
+      <div class="d-inline-block" style="font-size:3.9rem;font-weight:700;">US</div>
+      <div class="d-inline-block">
+          <h4>{{nTitle}}
+          <small *ngIf="nSubTitle"><br><small>{{nSubTitle}}</small></small>
+          </h4>
+      </div>
+      <div class="d-inline-block float-right" style="font-size:3.9rem;font-weight:700;margin-right:10px">
+        {{nYear}}
+      </div>
     </div>
   `
 })
 export class FormHeaderComponent {
   @Input('nTitle') nTitle: string;
   @Input('nSubTitle') nSubTitle: string;
+  @Input('nYear') nYear: number;
+
+  constructor(){
+    if (!this.nYear) this.nYear = new Date().getFullYear();
+  }
 }
